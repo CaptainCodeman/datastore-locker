bc125e9ea1e8108b932b6866f66743427a847fdf
Simon Green <simon@captaincodeman.com>
2016-07-22 12:10:02 -0600

    Add docs and canonical import

---
+++ docs.go
@@ -0,0 +1,6 @@
+package locker // import "github.com/captaincodeman/datastore-locker"
+
+/*
+locker provides a lock / lease mechanism to prevent duplicate execution
+of appengine tasks and an easy way to continue long-running processes
+*/
